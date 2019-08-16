04e6376fe1fcf0871fa0c2597a57189d97718f7b
David San Antonio <davsan56@gmail.com>
2019-08-16 16:03:03 -0400

    initial commit of setting up xcode project

---
+++ Kakuro/Kakuro/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Kakuro
+//
+//  Created by David San Antonio on 8/16/19.
+//  Copyright © 2019 PPC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
