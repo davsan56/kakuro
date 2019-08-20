c5af55205eb317c9078e867147f8bbd56465b73f
David San Antonio <davsan56@gmail.com>
2019-08-19 22:40:08 -0400

    Adding empty edge swiftui file

---
+++ Kakuro/Kakuro/Gameboard/EmptyEdge.swift
@@ -0,0 +1,28 @@
+//
+//  EmptyEdge.swift
+//  Kakuro
+//
+// Maybe eventually instead of being completely black
+// the squares could be invisible?
+//
+//  Created by David San Antonio on 8/19/19.
+//  Copyright © 2019 PPC. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmptyEdge: View {
+    var body: some View {
+        // Will probably have to vary the sizes based on size of board
+        Rectangle()
+            .frame(width: 75, height: 75, alignment: .center)
+    }
+}
+
+#if DEBUG
+struct EmptyEdge_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyEdge()
+    }
+}
+#endif
