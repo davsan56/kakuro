2a070770f0aa152a46a3df62d84f6043102c4a38
David San Antonio <davsan56@gmail.com>
2019-08-20 12:32:58 -0400

    Adding in potential ClueEdge

---
+++ Kakuro/Kakuro/Gameboard/ClueEdge.swift
@@ -0,0 +1,54 @@
+//
+//  ClueEdge.swift
+//  Kakuro
+//
+//  Created by David San Antonio on 8/20/19.
+//  Copyright © 2019 PPC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ClueEdge: View {
+    var bottomNumber: Int?
+    var topNumber: Int?
+    
+    var size = 75
+    
+    var body: some View {
+        ZStack {
+            if (bottomNumber != nil) {
+                Path { path in
+                    path.move(to: CGPoint(x: 0, y: 0))
+                    path.addLine(to: CGPoint(x: 0, y: size))
+                    path.addLine(to: CGPoint(x: size, y: size))
+                }
+                    .foregroundColor(.gray)
+                Text(String(bottomNumber!))
+                    .position(x: CGFloat(size / 4), y: CGFloat(Double(size) / 1.5))
+            }
+            
+            if (topNumber != nil) {
+                Path { path in
+                    path.move(to: CGPoint(x: 1, y: 0))
+                    path.addLine(to: CGPoint(x: size + 1, y: 0))
+                    path.addLine(to: CGPoint(x: size + 1, y: size))
+                }
+                    .foregroundColor(.gray)
+                Text(String(topNumber!))
+                    .position(x: CGFloat(Double(size) / 1.5), y: CGFloat(size / 4))
+            }
+        }
+        .frame(width: CGFloat(size), height: CGFloat(size), alignment: .center)
+    }
+}
+
+struct ClueEdge_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ClueEdge(bottomNumber: 43, topNumber: 7)
+            ClueEdge(bottomNumber: 5)
+            ClueEdge(topNumber: 32)
+        }
+        .previewLayout(.fixed(width: 100, height: 100))
+    }
+}
